ecdd0fbb15d5df9e054f276fa578690794c74c10
alexeyshaban <shaban@tut.by>
2013-10-05 19:11:03 -0700

    Functions in Java

---
+++ src/java8/lambda/Functions.java
@@ -0,0 +1,22 @@
+package java8;
+
+import java.util.function.Function;
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntUnaryOperator;
+
+public class Functions {
+    public static void main(String[] args) {
+        IntBinaryOperator add = (a, b) -> a + b;
+        System.out.println(add.applyAsInt(4, 5));
+        
+        IntUnaryOperator inc = a -> a + 1;
+        System.out.println(inc.applyAsInt(4));
+        
+        Function<Integer, IntUnaryOperator> addf = a -> b -> a + b;
+        System.out.println(addf.apply(9).applyAsInt(2));
+        
+        Function<IntBinaryOperator, Function<Integer, IntUnaryOperator>> applyf
+                = f -> a -> b -> f.applyAsInt(a, b);
+        System.out.println(applyf.apply(add).apply(33).applyAsInt(44));
+    }
+}
