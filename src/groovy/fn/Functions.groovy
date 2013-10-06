7f5896862fe3c6460040a183049b030b0704828a
alexeyshaban <shaban@tut.by>
2013-10-05 17:48:21 -0700

    +fibonaccif

--- src/groovy/fn/Functions.groovy
+++ src/groovy/fn/Functions.groovy
@@ -2,9 +2,18 @@ package groovy.fn
 
 class Functions {
     def add = {a, b ->  a + b}
-    
+
     def addf = {a -> {b -> a + b}}
-    
+
     def applyf = {f -> {a -> {b -> f(a, b)}}}
-    
+
+    def fibonaccif = {a, b ->
+        {
+            ->
+            def tmp = a;
+            a = b;
+            b += tmp;
+            return tmp;
+        }
+    }
 }
--- src/groovy/fn/FunctionsCaller.groovy
+++ src/groovy/fn/FunctionsCaller.groovy
@@ -7,3 +7,12 @@ println f.add(3, 4)
 println f.addf(8)(5)
 
 println f.applyf(f.add)(2)(7)
+
+def fib = f.fibonaccif(0, 1)
+print fib()
+print fib()
+print fib()
+print fib()
+print fib()
+print fib()
+print fib()
